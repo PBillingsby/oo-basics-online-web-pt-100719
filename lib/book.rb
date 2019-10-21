a2caac1cdfa7410cf20d9611a6759f463b883ef5
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-21 23:07:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,4 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  def initialize(title)
-    @title = title
-  end
-  
-  def title
-    @title
-  end
-  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  def initialize
 end
